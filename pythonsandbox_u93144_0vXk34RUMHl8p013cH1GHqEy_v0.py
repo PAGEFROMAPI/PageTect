bd440fff7a6ddcbaf08ab53b302183e48202b3ba
PAGEFROMAPI <pangnaboth.l@api-school.com>
2025-07-18 09:43:39 +0700

    Add files via upload

---
+++ pythonsandbox_u93144_0vXk34RUMHl8p013cH1GHqEy_v0.py
@@ -0,0 +1,12 @@
+import turtle
+t=turtle.Turtle()
+t.speed(5) #1:slowest, 3:slow, 5:normal, 10:fast, 0:fastest
+
+# create a subprogram to draw a square
+
+def drawcl():
+		t.circle(100)
+# call the subprogram
+drawcl()
+
+    
